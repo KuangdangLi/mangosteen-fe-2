b7534392e2c9cad0e55d7c3380df5e46b8f3b056
KuangdangLi <tosakarinmylove@gmail.com>
2023-03-24 22:24:24 +0800

    创建欢迎界面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,19 @@
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/First";
+import { Forth } from "../components/Forth";
+import { Second } from "../components/Second";
+import { Third } from "../components/Third";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
 
-export const routes = [
+export const routes:RouteRecordRaw[] = [
     {path:'/',component:Foo},
     {path:'/about',component:Bar},
+    {path:'/welcome',component:Welcome,children:[
+        {path:'first',component:First},
+        {path:'second',component:Second},
+        {path:'third',component:Third},
+        {path:'forth',component:Forth},
+    ]}
 ]
